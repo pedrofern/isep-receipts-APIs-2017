2a891c27b590c4dac5c7c34f0a3be79351718af8
Diana Silva <1151088@isep.ipp.pt>
2017-11-12 23:15:42 +0000

    Documentacao atualizada

--- ReceitasAPI/testes_unitarios/testes_pessoas.js
+++ ReceitasAPI/testes_unitarios/testes_pessoas.js
@@ -1,9 +1,10 @@
 var assert=require('assert');
 
 describe('GetPessoaId', function(){
-    function add(x,y);
+    function add(x,y){
         return x+y;
+    }
     it('should return 5', function(){
         assert.equal(add(2,3), 5);
-    }
-}
+    });
+});
